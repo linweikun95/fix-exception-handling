dcb77503e0ea6a35a6b948ea164b829db2ec860c
Bo Zhang <zhangbodut@gmail.com>
2019-08-04 11:32:03 +0800

    Sync

--- src/test/java/com/github/hcsp/exception/DatabaseReaderTest.java
+++ src/test/java/com/github/hcsp/exception/DatabaseReaderTest.java
@@ -1,7 +1,6 @@
 package com.github.hcsp.exception;
 
 import com.github.hcsp.test.helper.JavaASTReader;
-import com.github.hcsp.test.helper.ProjectSourceFileReader;
 import com.github.javaparser.ast.stmt.TryStmt;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
@@ -12,12 +11,6 @@ public class DatabaseReaderTest {
     public void hasFinallyOrTryWithResources() {
         List<TryStmt> tryStmts = JavaASTReader.findAll(DatabaseReader.class, TryStmt.class);
         Assertions.assertTrue(tryStmts.stream().anyMatch(this::hasFinallyOrTryWithResources));
-    }
-
-    @Test
-    public void hasClose() {
-        Assertions.assertTrue(
-                ProjectSourceFileReader.readAsString(DatabaseReader.class).contains(".close()"));
     }
 
     private boolean hasFinallyOrTryWithResources(TryStmt stmt) {
