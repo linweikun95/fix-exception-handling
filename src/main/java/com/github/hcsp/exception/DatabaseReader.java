e06ed61bd1f033c3c9a1baaeb9b954813ea7c487
Wang Zi-yi <zainking@foxmail.com>
2019-08-19 20:33:32 +0800

    Finish try catch in SQL (#9)
    Finish try catch in SQL (#9)

--- src/main/java/com/github/hcsp/exception/DatabaseReader.java
+++ src/main/java/com/github/hcsp/exception/DatabaseReader.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Objects;
 
 public class DatabaseReader {
     public static void main(String[] args) {
@@ -12,18 +14,54 @@ public class DatabaseReader {
         String jdbcUrl = "jdbc:h2:file:" + new File(projectDir, "test").getAbsolutePath();
         System.out.println(jdbcUrl);
 
-        Connection connection = DriverManager.getConnection(jdbcUrl, "sa", "");
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(jdbcUrl, "sa", "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                Objects.requireNonNull(connection).close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         PreparedStatement statement =
-                connection.prepareStatement("select * from PULL_REQUESTS where number > ?");
-        statement.setInt(1, 0);
-        ResultSet resultSet = statement.executeQuery();
-        while (resultSet.next()) {
-            System.out.println(
-                    resultSet.getInt(1)
-                            + " "
-                            + resultSet.getString(2)
-                            + " "
-                            + resultSet.getString(2));
+                null;
+        try {
+            statement = Objects.requireNonNull(connection).prepareStatement("select * from PULL_REQUESTS where number > ?");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            Objects.requireNonNull(statement).setInt(1, 0);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        ResultSet resultSet = null;
+        try {
+            resultSet = Objects.requireNonNull(statement).executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        while (true) {
+            try {
+                if (!Objects.requireNonNull(resultSet).next()) {
+                    break;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                System.out.println(
+                        Objects.requireNonNull(resultSet).getInt(1)
+                                + " "
+                                + resultSet.getString(2)
+                                + " "
+                                + resultSet.getString(2));
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
