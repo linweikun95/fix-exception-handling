17bdd678d486c3e49b21b6f3aaf6837ff06c2ef7
Moonstone <jackmoon1226@gmail.com>
2019-09-04 23:25:39 +0800

    handle exception (#20)
    handle exception (#20)

--- src/main/java/com/github/hcsp/exception/DatabaseReader.java
+++ src/main/java/com/github/hcsp/exception/DatabaseReader.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DatabaseReader {
     public static void main(String[] args) {
@@ -12,18 +13,32 @@ public class DatabaseReader {
         String jdbcUrl = "jdbc:h2:file:" + new File(projectDir, "test").getAbsolutePath();
         System.out.println(jdbcUrl);
 
-        Connection connection = DriverManager.getConnection(jdbcUrl, "sa", "");
-        PreparedStatement statement =
-                connection.prepareStatement("select * from PULL_REQUESTS where number > ?");
-        statement.setInt(1, 0);
-        ResultSet resultSet = statement.executeQuery();
-        while (resultSet.next()) {
-            System.out.println(
-                    resultSet.getInt(1)
-                            + " "
-                            + resultSet.getString(2)
-                            + " "
-                            + resultSet.getString(2));
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(jdbcUrl, "sa", "");
+            PreparedStatement statement =
+                    connection.prepareStatement("select * from PULL_REQUESTS where number > ?");
+            statement.setInt(1, 0);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println(
+                        resultSet.getInt(1)
+                                + " "
+                                + resultSet.getString(2)
+                                + " "
+                                + resultSet.getString(2));
+            }
+        } catch (SQLException e) {
+            throw new IllegalArgumentException(e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+
+                } catch (SQLException e) {
+                    throw new IllegalArgumentException(e);
+                }
+            }
         }
     }
 }
