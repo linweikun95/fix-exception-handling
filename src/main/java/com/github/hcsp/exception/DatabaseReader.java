521b904f9055772023d59cdc912ddebe3399bd05
Moon <justwe77@163.com>
2019-09-04 23:24:46 +0800

    optimize import

--- src/main/java/com/github/hcsp/exception/DatabaseReader.java
+++ src/main/java/com/github/hcsp/exception/DatabaseReader.java
@@ -1,7 +1,11 @@
 package com.github.hcsp.exception;
 
 import java.io.File;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DatabaseReader {
     public static void main(String[] args) {
