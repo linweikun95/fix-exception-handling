25b31f94479f63e9b8556377be0bb30b296ae028
zfhelo <51238700+zfhelo@users.noreply.github.com>
2019-12-01 13:29:47 +0800

    haah (#43)
    haah (#43)

--- src/main/java/com/github/hcsp/exception/DatabaseReader.java
+++ src/main/java/com/github/hcsp/exception/DatabaseReader.java
@@ -1,29 +1,42 @@
 package com.github.hcsp.exception;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class DatabaseReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         String jdbcUrl = "jdbc:h2:file:" + new File(projectDir, "test").getAbsolutePath();
         System.out.println(jdbcUrl);
-
-        Connection connection = DriverManager.getConnection(jdbcUrl, "sa", "");
-        PreparedStatement statement =
-                connection.prepareStatement("select * from PULL_REQUESTS where number > ?");
-        statement.setInt(1, 0);
-        ResultSet resultSet = statement.executeQuery();
-        while (resultSet.next()) {
-            System.out.println(
-                    resultSet.getInt(1)
-                            + " "
-                            + resultSet.getString(2)
-                            + " "
-                            + resultSet.getString(2));
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DriverManager.getConnection(jdbcUrl, "sa", "");
+            statement =
+                    connection.prepareStatement("select * from PULL_REQUESTS where number > ?");
+            statement.setInt(1, 0);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println(
+                        resultSet.getInt(1)
+                                + " "
+                                + resultSet.getString(2)
+                                + " "
+                                + resultSet.getString(2));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
         }
     }
 }
