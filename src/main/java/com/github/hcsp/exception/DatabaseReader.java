06bc126a741513251d02d5f73c895edbc7b176c5
hukai <huzhengen@163.com>
2020-01-17 15:18:18 +0800

    正确处理数据库连接的异常

--- src/main/java/com/github/hcsp/exception/DatabaseReader.java
+++ src/main/java/com/github/hcsp/exception/DatabaseReader.java
@@ -9,11 +9,7 @@ public class DatabaseReader {
         String jdbcUrl = "jdbc:h2:file:" + new File(projectDir, "test").getAbsolutePath();
         System.out.println(jdbcUrl);
 
-        Connection connection = null;
-        PreparedStatement statement = null;
-        try {
-            connection = DriverManager.getConnection(jdbcUrl, "sa", "");
-            statement = connection.prepareStatement("select * from PULL_REQUESTS where number > ?");
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, "sa", ""); PreparedStatement statement = connection.prepareStatement("select * from PULL_REQUESTS where number > ?")) {
             statement.setInt(1, 0);
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
@@ -23,13 +19,6 @@ public class DatabaseReader {
                                 + resultSet.getString(2)
                                 + " "
                                 + resultSet.getString(2));
-            }
-        } finally {
-            if (connection != null) {
-                connection.close();
-            }
-            if (statement != null) {
-                statement.close();
             }
         }
     }
