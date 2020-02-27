9bc3753fbd6b63a7320a7f14beb59e901494cc4b
bonnie1115 <49628449+bonnie1115@users.noreply.github.com>
2020-02-27 12:33:35 +0800

    处理db异常，关闭db链接 (#76)
    处理db异常，关闭db链接 (#76)

--- src/main/java/com/github/hcsp/exception/DatabaseReader.java
+++ src/main/java/com/github/hcsp/exception/DatabaseReader.java
@@ -1,29 +1,27 @@
 package com.github.hcsp.exception;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class DatabaseReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         String jdbcUrl = "jdbc:h2:file:" + new File(projectDir, "test").getAbsolutePath();
         System.out.println(jdbcUrl);
-
-        Connection connection = DriverManager.getConnection(jdbcUrl, "sa", "");
-        PreparedStatement statement =
-                connection.prepareStatement("select * from PULL_REQUESTS where number > ?");
-        statement.setInt(1, 0);
-        ResultSet resultSet = statement.executeQuery();
-        while (resultSet.next()) {
-            System.out.println(
-                    resultSet.getInt(1)
-                            + " "
-                            + resultSet.getString(2)
-                            + " "
-                            + resultSet.getString(2));
+        //try()  如果里面的方法继承了AutoCloseable则可如下写法
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, "sa", "");
+             PreparedStatement statement = connection.prepareStatement("select * from PULL_REQUESTS where number > ?")) {
+            statement.setInt(1, 0);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println(
+                        resultSet.getInt(1)
+                                + " "
+                                + resultSet.getString(2)
+                                + " "
+                                + resultSet.getString(2));
+            }
         }
     }
 }
+
