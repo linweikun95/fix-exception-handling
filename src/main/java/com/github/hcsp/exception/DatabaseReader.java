45e6a05655d816130bff43820b11d89cabad71e6
Sunpeng526 <55687152+Sunpeng526@users.noreply.github.com>
2020-03-11 01:28:38 -0700

    异常处理实战: 正确处理数据库连接的异常 (#83)
    异常处理实战: 正确处理数据库连接的异常 (#83)

--- src/main/java/com/github/hcsp/exception/DatabaseReader.java
+++ src/main/java/com/github/hcsp/exception/DatabaseReader.java
@@ -5,25 +5,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DatabaseReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         String jdbcUrl = "jdbc:h2:file:" + new File(projectDir, "test").getAbsolutePath();
         System.out.println(jdbcUrl);
 
-        Connection connection = DriverManager.getConnection(jdbcUrl, "sa", "");
-        PreparedStatement statement =
-                connection.prepareStatement("select * from PULL_REQUESTS where number > ?");
-        statement.setInt(1, 0);
-        ResultSet resultSet = statement.executeQuery();
-        while (resultSet.next()) {
-            System.out.println(
-                    resultSet.getInt(1)
-                            + " "
-                            + resultSet.getString(2)
-                            + " "
-                            + resultSet.getString(2));
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, "sa", "");
+                PreparedStatement statement = connection
+                        .prepareStatement("select * from PULL_REQUESTS where number > ?")) {
+            statement.setInt(1, 0);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(2));
+            }
         }
     }
 }
