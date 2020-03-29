1eeb71322b6b252bdcdde9f623f0ea2b4fad9b2a
bridgeren <240146036@qq.com>
2020-03-29 16:43:30 +0800

    简化代码â

--- src/main/java/com/github/hcsp/exception/DatabaseReader.java
+++ src/main/java/com/github/hcsp/exception/DatabaseReader.java
@@ -9,11 +9,7 @@ public class DatabaseReader {
         String jdbcUrl = "jdbc:h2:file:" + new File(projectDir, "test").getAbsolutePath();
         System.out.println(jdbcUrl);
 
-        Connection connection = null;
-        PreparedStatement statement = null;
-        try {
-            connection = DriverManager.getConnection(jdbcUrl, "sa", "");
-            statement = connection.prepareStatement("select * from PULL_REQUESTS where number > ?");
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, "sa", ""); PreparedStatement statement = connection.prepareStatement("select * from PULL_REQUESTS where number > ?")) {
             statement.setInt(1, 0);
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
@@ -26,13 +22,6 @@ public class DatabaseReader {
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.close();
-            }
-            if (statement != null) {
-                statement.close();
-            }
         }
 
     }
