818b527ef6aa00e732f01a0e6cb5b94c95a45e48
keliangyong <337215966@qq.com>
2020-04-09 23:51:21 +0800

    done (#102)
    done (#102)

--- src/main/java/com/github/hcsp/exception/DatabaseReader.java
+++ src/main/java/com/github/hcsp/exception/DatabaseReader.java
@@ -1,20 +1,18 @@
 package com.github.hcsp.exception;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class DatabaseReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         String jdbcUrl = "jdbc:h2:file:" + new File(projectDir, "test").getAbsolutePath();
         System.out.println(jdbcUrl);
 
-        Connection connection = DriverManager.getConnection(jdbcUrl, "sa", "");
-        PreparedStatement statement =
-                connection.prepareStatement("select * from PULL_REQUESTS where number > ?");
+        PreparedStatement statement;
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, "sa", "")) {
+            statement = connection.prepareStatement("select * from PULL_REQUESTS where number > ?");
+        }
         statement.setInt(1, 0);
         ResultSet resultSet = statement.executeQuery();
         while (resultSet.next()) {
